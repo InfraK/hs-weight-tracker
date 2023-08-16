8973d2abc0af9eb2cda1b5520045b5f82ed3dbd3
Santiago Kent <santiagokent@gmail.com>
2023-08-16 12:26:17 -0300

    feat: created a getAuthToken function that grabs the token from the header, refactored the Auth module to use qualified imports to solve some type isuses and readability

--- src/Lib/Auth.hs
+++ src/Lib/Auth.hs
@@ -1,17 +1,19 @@
-module Lib.Auth (encodeDecodePrint, sign, verify) where
+module Lib.Auth (encodeDecodePrint, sign, verify, privateJwk) where
 
 import Control.Monad
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Crypto.Random (MonadRandom)
-import qualified Data.Aeson as A
+import qualified Data.Aeson as Aeson
 import Data.Bifunctor (first)
-import Data.ByteString.Lazy (fromStrict, toStrict)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 import Data.Maybe (fromMaybe)
-import Data.Text
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 import Data.Time.Clock
 import Data.Time.Clock.POSIX
 import Jose.Jwa
-import Jose.Jwk
+import Jose.Jwk (Jwk, KeyUse (Sig), generateRsaKeyPair)
 import Jose.Jwt
 import Lib.Config (JwtConfig (JwtConfig))
 
@@ -21,12 +23,13 @@ data AuthData = AuthData
   deriving (Show)
 
 data TokenError
-  = TokenErrorMalformed String
+  = TokenNotFound
+  | TokenErrorMalformed String
   | TokenErrorExpired
   | TokenErrorUserIdNotFound
   deriving (Eq, Show)
 
-sign :: Jwk -> JwtConfig -> Text -> IO Jwt
+sign :: Jwk -> JwtConfig -> T.Text -> IO Jwt
 sign jwk (JwtConfig expMinutes _) sub = do
   claims <- makeJwtClaims sub (fromIntegral expMinutes)
   let payload = makePayload claims
@@ -36,29 +39,29 @@ sign jwk (JwtConfig expMinutes _) sub = do
       return jwt
     _ -> error "Could not sign JWT, check config"
 
-verify :: (MonadIO m, MonadRandom m) => Jwk -> Jwt -> m (Either TokenError AuthData)
+verify :: (MonadIO m, MonadRandom m) => Jwk -> T.Text -> m (Either TokenError AuthData)
 verify jwk jwt = do
-  eitherJwt <- decode [jwk] (Just encAlg) (unJwt jwt)
+  eitherJwt <- decode [jwk] (Just encAlg) (T.encodeUtf8 jwt)
   curTime <- liftIO getPOSIXTime
   return $ do
     jwtContent <- first (TokenErrorMalformed . show) eitherJwt
     claimsRaw <- case jwtContent of
       Jws (_, claimsRaw) -> Right claimsRaw
       _ -> Left $ TokenErrorMalformed "Not Jws"
-    jwtClaims <- first TokenErrorMalformed $ A.eitherDecode $ fromStrict claimsRaw
+    jwtClaims <- first TokenErrorMalformed $ Aeson.eitherDecode $ BL.fromStrict claimsRaw
     let (IntDate expiredAt) = fromMaybe (IntDate curTime) $ jwtExp jwtClaims
     when (expiredAt < curTime) $ Left TokenErrorExpired
     uid <- case jwtSub jwtClaims of
-      Just sub -> Right $ unpack sub
+      Just sub -> Right $ T.unpack sub
       Nothing -> Left TokenErrorUserIdNotFound
     return $ AuthData uid
 
-privateJwk :: Text -> IO Jwk
-privateJwk key = do
+privateJwk :: JwtConfig -> IO Jwk
+privateJwk (JwtConfig _ key) = do
   (_, privKey) <- generateRsaKeyPair 256 (KeyId key) Sig (Just (Signed RS256))
   return privKey
 
-makeJwtClaims :: Text -> NominalDiffTime -> IO JwtClaims
+makeJwtClaims :: T.Text -> NominalDiffTime -> IO JwtClaims
 makeJwtClaims sub expMinutes = do
   currentUTC <- getCurrentTime
   let expiry = IntDate $ utcTimeToPOSIXSeconds $ addUTCTime (expMinutes * 60) currentUTC
@@ -73,7 +76,7 @@ makeJwtClaims sub expMinutes = do
       Nothing
 
 makePayload :: JwtClaims -> Payload
-makePayload claims = Claims $ toStrict $ A.encode claims
+makePayload claims = Claims $ BL.toStrict $ Aeson.encode claims
 
 encAlg :: JwtEncoding
 encAlg = JwsEncoding RS256
@@ -81,12 +84,16 @@ encAlg = JwsEncoding RS256
 encodeDecodePrint :: IO ()
 encodeDecodePrint = do
   let config = JwtConfig 60 "myawesomekey"
-  jwk <- privateJwk "myawesomekey"
+  jwk <- privateJwk config
   jwt <- sign jwk config "uuid"
   print jwt
-  content <- verify jwk jwt
+  print $ unJwt jwt
+  content <- verify jwk (bsToText $ unJwt jwt)
   case content of
     Right (AuthData uid) -> do
       print uid
     (Left err) -> do
       print err
+
+bsToText :: B.ByteString -> T.Text
+bsToText = T.decodeUtf8
--- src/Lib/Routes.hs
+++ src/Lib/Routes.hs
@@ -3,44 +3,69 @@
 module Lib.Routes (routes) where
 
 import Control.Monad.IO.Class (MonadIO (liftIO))
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import Database.PostgreSQL.Simple (Connection)
 import Lib.User (CreateUser (CreateUser), createUser, findUser, findUsers)
 import Lib.Weight (CreateWeight (CreateWeight), createWeight, findWeights)
 import Network.Wai.Middleware.RequestLogger (logStdout)
 import Web.Scotty
-  ( ScottyM,
+  ( ActionM,
+    ScottyM,
     get,
+    header,
     json,
     jsonData,
     middleware,
     param,
     post,
   )
-import Database.PostgreSQL.Simple (Connection)
 
 routes :: Connection -> ScottyM ()
 routes conn = do
   middleware logStdout
+
   -- Users
   get "/users" $ do
     users <- liftIO $ findUsers conn
-    json $ users
+    json users
 
   get "/users/:uid" $ do
+    authHeader <- header "Authorization"
+    liftIO $ print authHeader
+    authToken <- getAuthToken
+    liftIO $ print authToken
+
     uid <- param "uid"
     [user] <- liftIO $ findUser conn uid
-    json $ user
+    json user
 
   post "/users" $ do
     (CreateUser email) <- jsonData
     [user] <- liftIO $ createUser conn (CreateUser email)
-    json $ user
+    json user
 
   -- Weights
   get "/users/:uid/weights" $ do
     weights <- liftIO $ findWeights conn
-    json $ weights
+    json weights
 
   post "/users/:uid/weights" $ do
     (CreateWeight grams) <- jsonData
     [weight] <- liftIO $ createWeight conn (CreateWeight grams)
-    json $ weight
+    json weight
+
+data AuthError
+  = TokenNotFound
+  deriving (Eq, Show)
+
+type Token = T.Text
+
+getAuthToken :: ActionM (Either AuthError Token)
+getAuthToken = do
+  maybeHeader <- header "Authorization"
+  case maybeHeader of
+    Just str -> do
+      let token = TL.toStrict $ last $ TL.words str
+      return $ Right token
+    Nothing -> return $ Left TokenNotFound
